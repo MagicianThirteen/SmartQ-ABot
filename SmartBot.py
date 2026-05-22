3d3787e8b9fb29eec301514e3f460171163adc78
zhangyuan <ZARLZYJILL@163.com>
2026-05-22 20:27:32 +0800

    定义要结构化输出的模板

--- SmartBot.py
+++ SmartBot.py
@@ -12,3 +12,18 @@ if os.getenv("LANGSMITH_API_KEY"):
     os.environ["LANGSMITH_TRACING"]="true"
     os.environ.setdefault("LANGSMITH_PROJECT","SmartQABot")
     print(f"LangSmith配置完成-Project:{os.getenv('LANGSMITH_PROJECT')}")
+
+#定义要输出的结构 Schema
+#全用英文，更省token
+class QAResponse(BaseModel):
+    answer:str=Field(description="The answer to the user's question")
+    confidence:str=Field(description="Confidence level:high,medium,or low")
+    reasoning:str=Field(description="The reasoning behind the answer provided.")
+    follow_up_questions:List[str]=Field(
+        description="A list of follow-up questions related to the topic.",
+        default_factory=list,
+    )
+    sources_needed:bool =Field(
+        description="Indicates whether sources are needed for the answer.",
+        default=False,
+    )
